b55352904496f5513a31f1849dc5ae632b110cfc
liteodoro <teodorolilisilva@gmail.com>
2022-03-09 14:09:44 -0300

    inicio projeto

--- aula.js
+++ aula.js
@@ -1,1 +1,67 @@
-console.log('bem vindo ao curso')
+//let idade = 5;
+//console.log(idade)
+//let altura = 155
+//console.log(altura)
+
+//let nome = 'aline';
+//console.log(nome)
+
+let sexo = 'feminino';
+let corOlhos = 'castanhos';
+let corCabelos = 'preto';
+
+//console.log(sexo, corOlhos, corCabelos)
+
+//segunda parte da aula
+
+//let valorIngressoadulto = 20;
+//valorIngressoadulto = 30;
+//console.log(valorIngressoadulto);
+
+//tipos Primitivos javascript
+
+//sã0 dois referencia e valores
+
+//referencia  number, string, boolean 
+
+
+let nome = 'Aline'; //string literal varias letras em uma palavra
+let idade = 42; //number literal
+let estaAprovado = true; //boolean
+let sobrenome = undefined ; //undefine sem tipo
+//let corSelecionado = null; //redefinir valor
+
+//console.log(nome, idade, estaAprovado, sobrenome, corSelecionado)
+
+// tipagem Dinâmica
+
+//obejeto 
+
+let pessoa = {
+     nome: 'Aline',
+     idade : 42,
+     estaAprovado : true,
+     sobrenome : 'Silva Teodoro',
+     corOlhos : 'castanho',
+     corCabelo : 'preto',
+};
+
+ console.log(pessoa);
+
+ //arrays
+
+ let familia = [26, 45, 50, 17];
+console.log(familia.length);
+ console.log(familia[0]);
+
+ //functions sem parametros ()vazio
+
+ //verbo + substantivo
+
+ let corSite = "azul";
+
+ function resetarCor(cor){ //quando coloca parametros
+     corSite = "cor";
+ };
+
+console.log()
